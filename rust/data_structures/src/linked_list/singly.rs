f4b1333c73a2305c1ebe0734d922c81dab558e7e
Minh Quan Do <mdo6180@gmail.com>
2023-01-28 20:44:05 -0500

    implemented display trait for SLLNode

--- rust/data_structures/src/linked_list/singly.rs
+++ rust/data_structures/src/linked_list/singly.rs
@@ -1,17 +1,19 @@
 use std::option::Option;
+use std::fmt;
 
 pub struct SLLNode {
-    pub value: i32,
+    pub value: Option<i32>,
     pub next: Option<Box<SLLNode>>,
 }
 
 pub struct SinglyLinkedList {
     pub head: Option<Box<SLLNode>>,
-    pub num_elements: i32
+    pub num_elements: i32,
+    pub sorted: bool
 }
 
 impl SLLNode {
-    pub fn new(value: i32, next: Option<Box<SLLNode>>) -> SLLNode {
+    pub fn new(value: Option<i32>, next: Option<Box<SLLNode>>) -> SLLNode {
         SLLNode {
             value: value,
             next: next
@@ -19,14 +21,117 @@ impl SLLNode {
     }
 }
 
+impl fmt::Display for SLLNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.value {
+            Some(val) => {
+                write!(f, "Node({})", val)
+            },
+            None => {
+                write!(f, "Node(None)")
+            }
+        }
+    }
+}
+
 impl SinglyLinkedList {
-    pub fn new() -> SinglyLinkedList {
+    pub fn new(head: Option<Box<SLLNode>>, sorted: bool) -> SinglyLinkedList {
         SinglyLinkedList {
-            head: None,
-            num_elements: 0
+            head: head,
+            num_elements: 0,
+            sorted: sorted
+        } 
+    }
+
+    pub fn insert(&mut self, value: i32) {
+        if self.head.is_none() {
+            self.head = Some(
+                Box::new(
+                    SLLNode::new(Some(value), None)
+                )
+            );
+            self.num_elements += 1;
+        
+        } else {
+            {
+                let mut current = self.head.as_mut();
+
+                loop {
+                    match current {
+                        Some(node) => {
+                            // checks if the next node is None,
+                            // if it is not None, set current to a &mut of the next node.
+                            // if it is None, we are at the end of the list, 
+                            // thus, set the next parameter to another node
+                            if node.next.is_some() {
+                                current = node.next.as_mut();
+                            } else {
+                                node.next = Some(Box::new(SLLNode::new(None, None)));
+                                self.num_elements += 1;
+                                break;
+                            }
+                        },
+                        None => {
+                            break;
+                        }
+                    };
+                }
+            }
+            
+            if self.sorted {
+                /*
+                let mut current = self.head.as_mut();
+                let mut current_val: Option<i32> = None;
+
+                loop {
+                    match current {
+                        Some(node) => {
+
+                            match &node.next {
+                                Some(next) => {
+
+                                    println!("Node = {}", next);
+                                    current = node.next.as_mut();
+                                },
+                                None => {
+                                    println!("Nothing");
+                                }
+                            }
+                        },
+                        None => {
+                            break;
+                        }
+                    };
+                }
+                */
+
+            } else {
+                let mut current = self.head.as_mut();
+
+                loop {
+                    match current {
+                        Some(node) => {
+                            
+                            match node.value {
+                                Some(_val) => {
+                                    current = node.next.as_mut();
+                                },
+                                None => {
+                                    node.value = Some(value);
+                                    break;
+                                }
+                            }
+                        },
+                        None => {
+                            break;
+                        }
+                    };
+                }
+            }
         }
     }
 
+    /*
     pub fn insert(&mut self, value: i32) {
         // is_none() method is part of std::option::Option
         if self.head.is_none() {
@@ -41,28 +146,83 @@ impl SinglyLinkedList {
             // use .as_mut() to create a &mut (mutable reference) to the Box value (aka the head)
             let mut current = self.head.as_mut();
 
-            loop {
-                match current {
-                    Some(node) => {
-                        // checks if the next node is None,
-                        // if it is not None, set current to a &mut of the next node.
-                        // if it is None, we are at the end of the list, 
-                        // thus, set the next parameter to another node
-                        if node.next.is_some() {
-                            current = node.next.as_mut();
-                        } else {
-                            node.next = Some(Box::new(SLLNode::new(value, None)));
-                            self.num_elements += 1;
+            if self.sorted {
+                loop {
+                    match current {
+                        Some(node) => {
+                            // checks if the next node is None,
+                            // if it is not None, set current to a &mut of the next node.
+                            // if it is None, we are at the end of the list, 
+                            // thus, set the next parameter to another node
+                            
+                            match &node.next {
+                                Some(next) => {
+                                    if next.value > value {
+                                        /*
+                                        let mut new_node = Box::new(SLLNode::new(value, None)); 
+                                        new_node.next = node.next;
+                                        println!("{} is larger than {}", next.value, value);
+                                        */
+                                        //let mut next_node = next;
+                                        let mut new_node = Box::new(SLLNode::new(value, None));  
+                                        new_node.next = Some(next.as_mut());
+                                        node.next = Some(new_node);
+                                        //new_node.next = current.unwrap();
+                                        break;
+                                    }
+                                    current = node.next.as_mut();
+                                },
+                                None => {
+                                    node.next = Some(Box::new(SLLNode::new(value, None)));
+                                    self.num_elements += 1;
+                                    break;
+                                }
+                            }
+                            
+                            /*
+                            if node.next.is_some() {
+                                current = node.next.as_mut();
+                            } else {
+                                node.next = Some(Box::new(SLLNode::new(value, None)));
+                                self.num_elements += 1;
+                                break;
+                            }
+                            */
+                        },
+                        None => {
                             break;
                         }
-                    },
-                    None => {
-                        break;
-                    }
-                };
+                    };
+                }
+
+                //let &mut n = current.unwrap();
+                //println!("current = {}", n.value);
+
+            } else {
+                loop {
+                    match current {
+                        Some(node) => {
+                            // checks if the next node is None,
+                            // if it is not None, set current to a &mut of the next node.
+                            // if it is None, we are at the end of the list, 
+                            // thus, set the next parameter to another node
+                            if node.next.is_some() {
+                                current = node.next.as_mut();
+                            } else {
+                                node.next = Some(Box::new(SLLNode::new(value, None)));
+                                self.num_elements += 1;
+                                break;
+                            }
+                        },
+                        None => {
+                            break;
+                        }
+                    };
+                }
             }
         }
     }
+    */
 
     pub fn print_list(&self) {
         if self.head.is_some() {
@@ -72,7 +232,7 @@ impl SinglyLinkedList {
             loop {
                 match current {
                     Some(node) => {
-                        println!("element {} = {}", position, node.value);
+                        println!("element {} = {}", position, node.to_string());
                         current = &node.next;
                         position += 1;
                     },
--- rust/data_structures/src/main.rs
+++ rust/data_structures/src/main.rs
@@ -1,20 +1,25 @@
 mod linked_list;
-//use linked_list::singly::{SinglyLinkedList, SLLNode};
-use linked_list::singly::SinglyLinkedList;
+use linked_list::singly::{SinglyLinkedList, SLLNode};
+//use linked_list::singly::SinglyLinkedList;
 
 
 fn main() {
-    let mut list: SinglyLinkedList = SinglyLinkedList::new();
-    //let node3: SLLNode = SLLNode::new(3, None);
-    //let node2: SLLNode = SLLNode::new(2, Some(Box::new(node3)));
-    //let node1: SLLNode = SLLNode::new(1, Some(Box::new(node2)));
-    //list.head = Some(Box::new(node1));
+    let node3: SLLNode = SLLNode::new(Some(3), None);
+    let node2: SLLNode = SLLNode::new(Some(2), Some(Box::new(node3)));
+    let node1: SLLNode = SLLNode::new(Some(1), Some(Box::new(node2)));
+    let mut list: SinglyLinkedList = SinglyLinkedList::new(Some(Box::new(node1)), false);
+    //let mut list: SinglyLinkedList = SinglyLinkedList::new(None, true);
 
     list.insert(1);
+    list.print_list();
+
+    /*
+    list.insert(1);
     list.insert(2);
-    list.insert(3);
-    list.insert(4);
     list.insert(5);
     list.insert(6);
+    list.insert(3);
+    list.insert(4);
     list.print_list();
+    */
 }
