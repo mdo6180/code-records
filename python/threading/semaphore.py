073fd03150ba8d4b42d7d6e256a8732921cc8e2c
Minh Quan Do <mdo6180@gmail.com>
2023-08-22 17:29:21 -0400

    added some comments in semaphore.py

--- python/threading/semaphore.py
+++ python/threading/semaphore.py
@@ -2,7 +2,14 @@ import threading
 import time
 
 # Create a counting semaphore
-num_connections = 3  # Number of available connections
+
+# if value of semaphore is less than the number of child threads (suppose we set the value to 3 i.e., we set num_connections = 3), 
+# threads 4 and 5 will have to wait for threads 1, 2, or 3 to release the semaphore before it can acquire the semaphore and start its execution.
+
+# if value of semaphore is equal to the number of child threads, then all threads can acquire the semaphore and execute all at once.
+ 
+# num_connections = 3  # Number of available connections
+num_connections = 5
 semaphore = threading.Semaphore(value=num_connections)
 
 # Simulated function that uses a shared resource (database connection)
