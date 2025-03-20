51624362c98cc7b5ee539a92a1c7ae4d32945c4b
Minh Quan Do <mdo@mitre.org>
2025-03-20 19:01:06 -0400

    added example of js bridge for multiple connections with htmx

---
+++ htmx/sse/multiple_connections/js_bridge/static/main.js
@@ -0,0 +1,19 @@
+document.body.addEventListener('htmx:sseOpen', (event) => {
+    const source = event.detail.source;
+    console.log(source);
+});
+
+
+document.body.addEventListener('htmx:sseBeforeMessage', (event) => {
+    const elt = event.detail.elt;   // this is the element that has the sse-connect attribute
+    
+    if (event.detail.type === "ChangeColor") {
+        event.preventDefault();     // call preventDefault() on the event to prevent the sse-swap from swapping in the data
+
+        const data = JSON.parse(event.detail.data);
+        console.log(data);
+
+        const element = document.getElementById("title");
+        element.style.color = data.color;
+    }
+});
