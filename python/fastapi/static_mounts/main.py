cc3c51e51283b64813632490c9e51b9a838d890e
Minh Quan Do <mdo@mitre.org>
2025-06-06 15:09:23 -0400

    added style sheet for main app

--- python/fastapi/static_mounts/main.py
+++ python/fastapi/static_mounts/main.py
@@ -15,6 +15,7 @@ app2.mount("/static", StaticFiles(directory="./static2"), name="static")
 app = FastAPI()
 app.mount("/app1", app1)
 app.mount("/app2", app2)
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
@@ -25,12 +26,19 @@ async def home():
             <meta charset="UTF-8">
             <title>Multiple StaticFiles</title>
 
+            <!-- Static files for app1 -->
             <link rel="stylesheet" href="/app1/static/style.css">
+
+            <!-- Static files for app2 -->
             <link rel="stylesheet" href="/app2/static/style.css">
+
+            <!-- Main static files -->
+            <link rel="stylesheet" href="/static/style.css">
         </head>
         <body>
             <div id="app1">App 1</div>
             <div id="app2">App 2</div>
+            <div id="main">Main</div>
         </body>
     </html> 
     """
