8373dfc1e9525db4ddab1d06ef8cd20a088408e4
Minh Quan Do <mdo@mitre.org>
2025-08-28 18:07:27 -0400

    added line plot example for D3.js

---
+++ htmx/d3/line-plot/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+
+newline = "\n"
+app = FastAPI()
+app.mount("/js", StaticFiles(directory="./js"), name="js")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return f"""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>HTMX Indicator</title>
+
+            <!-- non-minified Htmx -->
+            <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.js" integrity="sha384-ksKjJrwjL5VxqAkAZAVOPXvMkwAykMaNYegdixAESVr+KqLkKE8XBDoZuwyWVUDv" crossorigin="anonymous"></script>
+
+            <!-- Add more dependencies here -->
+        </head>
+        <body>
+            <!-- Load d3.js -->
+            <script src="https://d3js.org/d3.v6.js"></script>
+
+            <!-- Create a div where the graph will take place -->
+            <div id="my_dataviz"></div>
+            
+            <script src="/js/line_chart.js"></script>
+        </body>
+    </html>
+    """
