3ad26de502cdf699279d48e385961660c5fe0231
Minh Quan Do <mdo6180@gmail.com>
2026-06-17 18:03:17 -0400

    changed to more advanced __getitem__ example

--- python/basics/magic_methods.py
+++ python/basics/magic_methods.py
@@ -1,34 +1,37 @@
-class Dictionary:
-    def __init__(self, data):
+from typing import List, Tuple, Union
+
+
+
+class Matrix:
+    def __init__(self, data: List[List[int]]):
         self.data = data
 
-    def __getitem__(self, key):
-        return self.data[key]
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.data[index]
 
-    def __setitem__(self, key, value):
-        self.data[key] = value
+        elif isinstance(index, tuple):
+            row, col = index
 
-    def __delitem__(self, key):
-        del self.data[key]
+            # matrix[0:3, 2]
+            if isinstance(row, slice) and isinstance(col, int):
+                return [r[col] for r in self.data[row]]
 
-    def __contains__(self, key):
-        return key in self.data
-
-    def __len__(self):
-        return len(self.data)
-
-    def __iter__(self):
-        return iter(self.data)
-
-    def __repr__(self):
-        return f"Dictionary({self.data})"
+            # matrix[1, 2]
+            elif isinstance(row, int) and isinstance(col, int):
+                return self.data[row][col]
 
 
-dictionary = Dictionary({'a': 1, 'b': 2, 'c': 3})
-print(dictionary['a'])  # Output: 1
-dictionary['d'] = 4
-print(dictionary)  # Output: Dictionary({'a': 1, 'b': 2, 'c': 3, 'd': 4})
-del dictionary['b']
-print(dictionary)  # Output: Dictionary({'a': 1, 'c': 3, 'd': 4})
-print('c' in dictionary)  # Output: True
-print(len(dictionary))  # Output: 3
+matrix = Matrix(
+    [[1, 2, 3], 
+     [4, 5, 6], 
+     [7, 8, 9]]
+)
+print(matrix[0])
+print(matrix[0, 1])
+print(matrix[0:3, 2])
+
+
+# Note: for Anacostia, we can use the __getitem__ method like so: stream[hash, "location"] to get the location associated with the hash key in the stream.
+# We can also do stream[hash, "content"] to get the content associated with the hash key in the stream. 
+# This allows us to easily access specific pieces of information from the stream using a simple and intuitive syntax.
