178249e3fc49ddaf2939dc1d1f108ec3d1185de9
Minh Quan Do <mdo6180@gmail.com>
2026-07-23 01:23:11 -0400

    added script to query schema of sqlite database

---
+++ sqlite/attach_db/schema_query.sql
@@ -0,0 +1,19 @@
+-- Attach the low-side database
+ATTACH DATABASE 'low_side.db' AS low;
+
+BEGIN;
+
+-- display the schema of the low-side database
+SELECT * FROM low.sqlite_schema;
+
+-- display the schema of the low-side database, filtering out internal SQLite tables and ensuring that only tables with valid SQL definitions are shown
+SELECT name, sql
+FROM low.sqlite_schema
+WHERE type = 'table'
+    AND name NOT LIKE 'sqlite_%'
+    AND sql IS NOT NULL
+ORDER BY name;
+
+COMMIT;
+
+DETACH DATABASE low;
